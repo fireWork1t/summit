c90e63e701a71447ac8a67d85240844aa8326884
fireWork1t <tagewilson@gmail.com>
2024-03-26 17:06:03 -0700

    Finished basic system

--- index.js
+++ index.js
@@ -1,117 +1,135 @@
-var express = require('express');
-var app = express();
-var redis = require('redis');
-var redisClient;
+var express = require('express')
+var app = express()
+var cors = require('cors') // Import the cors package
+
+app.use(cors())
+
+var redis = require('redis')
+var redisClient
 var bodyParser = require('body-parser')
-const NODECACHE = require( "node-cache" );
-const QUEUECACHE = new NODECACHE();
-const LIFTDATA = require('./data/lift-data.json');
-const LIFTS = LIFTDATA.liftData;
+const NODECACHE = require('node-cache')
 
-// parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: false }))
+const QUEUECACHE = new NODECACHE({ stdTTL: 10800 })
+const LIFTDATA = require('./data/lift-data.json')
+const LIFTS = LIFTDATA.liftData
 
-// parse application/json
+const AVERAGE_WAIT_TIME_EXPIRATION = 10800
+const USER_DATA_EXPIRATION = 600
+
+// const waitTimeMapping = {
+//   '0 minutes': 0,
+//   '1 minute': 1,
+//   '3 minutes': 3,
+//   '5 minutes': 5,
+//   '7 minutes': 7,
+//   '9 minutes': 9,
+//   '11 minutes': 11,
+//   '13 minutes': 13,
+//   '15 minutes': 15,
+//   'over 15 minutes': 20
+// }
+
 app.use(bodyParser.json())
 
-var calculationInterval = setInterval(
-    function(){
-    
-    calculate();
+var calculationInterval = setInterval(function () {
+  calculate()
+}, 5000)
 
-
-  }, 5000);
-
-async function connectRedis(){
-    redisClient = redis.createClient();
-    redisClient.on('error', err => console.log('Redis Client Error', err));
-    await redisClient.connect();
+async function connectRedis () {
+  redisClient = redis.createClient()
+  redisClient.on('error', err => console.log('Redis Client Error', err))
+  await redisClient.connect()
 }
 
+const asyncHandler = fun => (req, res, next) => {
+  Promise.resolve(fun(req, res, next)).catch(next)
+}
 
-const asyncHandler = (fun) => (req, res, next) => {
-    Promise.resolve(fun(req, res, next))
-      .catch(next)
-  }
-
-app.post('/collectData', asyncHandler(async function (req, res) {
+app.post(
+  '/update',
+  asyncHandler(async function (req, res) {
     if (!redisClient) {
-        await connectRedis();
+      await connectRedis()
     }
 
-    await redisClient.set(`${req.body.lift}:${req.body.uniqueID}`, 1, {EX: 600});
-    console.log({req})
-    
-
-    
-    res.end(JSON.stringify(await getQueueSize())); 
-    
-    
- }))
-
-
- async function calculate(){
-    
-    for (const lift of LIFTS){
-        let liftQueueSize = await redisClient.eval(`return #redis.pcall('keys', '${lift.name}:*')`);
-        
-        QUEUECACHE.set(lift.name, liftQueueSize);
-        
+    if (!Number.isInteger(req.body.waitTime)) {
+      return res.send({ error: 'Invalid input' })
     }
-    
-    
-}
 
-async function getQueueSize(){
-    
-    let liftQueues = {};
-    for (const lift of LIFTS){
-         
-        liftQueues[lift.name] = Math.round(QUEUECACHE.get(lift.name)/lift.peoplePerMinute);
+    await redisClient.set(
+      `${req.body.lift}:${req.body.uniqueID}`,
+      req.body.waitTime,
+      { EX: USER_DATA_EXPIRATION }
+    )
+
+    return res.send({ message: 'ok' })
+  })
+)
+
+app.get(
+  '/status/:skiArea',
+  asyncHandler(async function (req, res) {
+    res.end(JSON.stringify(await getQueueSize(req.params.skiArea)))
+  })
+)
+
+async function calculate () {
+  for (const lift of LIFTS) {
+    let liftQueueSize = await redisClient.keys(`${lift.name}:*`)
+
+    let total = 0
+    let average = 0
+    let userDataFound = false
+
+    for (const key of liftQueueSize ?? []) {
+      total += Number(await redisClient.get(key))
+
+      if (!userDataFound && key != `${lift.name}:average1`) {
+        userDataFound = true
+      }
     }
-    return liftQueues;
-    
-}
 
-function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-     var R = 6371; // Radius of the earth in km
-     var dLat = deg2rad(lat2-lat1); // deg2rad below
-     var dLon = deg2rad(lon2-lon1);
-     var a =
-         Math.sin(dLat/2) * Math.sin(dLat/2) +
-         Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-         Math.sin(dLon/2) * Math.sin(dLon/2)
-         ;
-     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-     var d = R * c * 1000; // Distance in m
-     return d;
+    //total += QUEUECACHE.get(lift.name)
+
+    //console.log({ added: QUEUECACHE.get(lift.name), lift: lift.name })
+
+    //console.log(lift.name + ' ' + total)
+    average = Math.round(total / liftQueueSize.length)
+
+    if (average >= 0 && userDataFound) {
+      await redisClient.set(`${lift.name}:${'average1'}`, average, {
+        EX: USER_DATA_EXPIRATION
+      })
+
+      QUEUECACHE.set(lift.name, average)
+
+      await redisClient.set(lift.skiArea + ':' + lift.name, average, {
+        EX: AVERAGE_WAIT_TIME_EXPIRATION
+      })
     }
-// Convert degrees to radians
-function deg2rad(deg) {  
-    return deg * (Math.PI/180)
-}
-
-// Run calculation function and return results
-function checkPositionFrom(latitude, longitude, radius){
-    var result = document.getElementById("result");
-    if (getDistanceFromLatLonInKm(x.innerHTML,y.innerHTML,latitude, longitude).toFixed(1) < radius) {
-         
-         result.innerHTML = "Your distance from " + latitude + ", " + longitude + " is " + (getDistanceFromLatLonInKm(x.innerHTML,y.innerHTML,latitude, longitude).toFixed(1)) + " meters, which is inside the set radius of " + (radius) + " meters" ;
-         // Return results if inside radius
-         }
-    else {
-         result.innerHTML = "Your distance from " + latitude + ", " + longitude + " is " + (getDistanceFromLatLonInKm(x.innerHTML,y.innerHTML,latitude, longitude).toFixed(1)) + " meters, which is outside the set radius of " + (radius) + " meters" ;
-         // Return results if outside radius
-         }
-    
-  
   }
+}
 
- var server = app.listen(8081, async function () {
-    await connectRedis();
-    var host = server.address().address
-    var port = server.address().port
-    console.log("Listening at http://%s:%s", host, port)
- }) 
+async function getQueueSize (skiArea) {
+  let liftQueues = {}
+  let filteredQueues = LIFTS.filter(x => x.skiArea == skiArea) // filters lift cache to only include lifts from the selected ski area
 
- 
+  //console.log({ filteredQueues, skiArea })
+  for (const lift of filteredQueues) {
+    let tempWaitTime = QUEUECACHE.get(lift.name)
+    if (!tempWaitTime) {
+      tempWaitTime = await redisClient.get(lift.skiArea + ':' + lift.name)
+      QUEUECACHE.set(lift.name, tempWaitTime)
+    }
+    liftQueues[lift.name] = tempWaitTime
+  }
+  //console.log(liftQueues)
+  return liftQueues
+}
+
+var server = app.listen(8080, async function () {
+  await connectRedis()
+  var host = server.address().address
+  var port = server.address().port
+  console.log('Listening at http://%s:%s', host, port)
+})
--- test/calculation.test.js
+++ test/calculation.test.js
@@ -1,68 +1,61 @@
-const axios = require('axios');
-const liftData = require('../data/lift-data.json');
-const lifts = liftData.liftData;
-var redisClient;
-var redis = require('redis');
+const axios = require('axios')
+const liftData = require('../data/lift-data.json')
+const lifts = liftData.liftData
+var redisClient
+var redis = require('redis')
 
 describe('collectData endpoint', () => {
-    beforeAll(async () => {
-        redisClient = redis.createClient();
-        await redisClient.connect();
-        await redisClient.FLUSHALL();
-        await redisClient.QUIT();
-        uniqueIDs = generateUniqueIDs(100); // Generate 100 random unique IDs
-    
-      });
-    
-      afterAll(async() => {
-        await sleep(5);
-        const response = await axios.post('http://localhost:8081/collectData', {
-              lift: lifts[0].name,
-              uniqueID: 2
-            });
-            
-        for (const lift of lifts) {
-            expect(response.data[lift.name]).toBe(Math.round(100/lift.peoplePerMinute));
-        }
-        
-      },8000);
-    
+  beforeAll(async () => {
+    redisClient = redis.createClient()
+    await redisClient.connect()
+    await redisClient.FLUSHALL()
+    await redisClient.QUIT()
+    uniqueIDs = generateUniqueIDs(2) // Generate 100 random unique IDs
+  })
 
-  let uniqueIDs;
+  afterAll(async () => {
+    await sleep(5)
+    const response = await axios.get('http://localhost:8080/status')
+    console.log(JSON.stringify(response.data))
 
+    for (const lift of lifts) {
+      expect(response.data[lift.name] > 0).toBeTruthy()
+    }
+  }, 8000)
 
-  test.each(lifts)('should collect data for lift %s', async (lift) => {
+  let uniqueIDs
+
+  test.each(lifts)('should collect data for lift %s', async lift => {
     for (const uniqueID of uniqueIDs) {
       try {
-        const response = await axios.post('http://localhost:8081/collectData', {
+        const response = await axios.post('http://localhost:8080/update', {
           lift: lift.name,
-          uniqueID: uniqueID
-        });
-  
-        console.log(`Response for ${lift.name}:${uniqueID}:`, response.data);
-        expect(response.status).toBe(200);
+          uniqueID: uniqueID,
+          waitTime: Math.round(Math.random() * 10)
+        })
+
+        console.log(`Response for ${lift.name}:${uniqueID}`, response.data)
+        expect(response.status).toBe(200)
         // Add more assertions as needed
       } catch (error) {
-        console.error(`Error for ${lift.name}:${uniqueID}:`, error);
-        throw error;
+        console.error(`Error for ${lift.name}:${uniqueID}`, error)
+        throw error
       }
     }
-  });
-},8000);
-
-
+  })
+}, 8000)
 
 // Helper function to generate an array of random unique IDs
-function generateUniqueIDs(count) {
-  const uniqueIDs = [];
-  
+function generateUniqueIDs (count) {
+  const uniqueIDs = []
+
   for (let i = 0; i < count; i++) {
-    const uniqueID = Math.random().toString(36).substring(7);
-    uniqueIDs.push(uniqueID);
+    const uniqueID = Math.random().toString(36).substring(7)
+    uniqueIDs.push(uniqueID)
   }
-  
-  return uniqueIDs;
+
+  return uniqueIDs
 }
-async function sleep(seconds) {
-    return new Promise((resolve) =>setTimeout(resolve, seconds * 1000));
-    }
+async function sleep (seconds) {
+  return new Promise(resolve => setTimeout(resolve, seconds * 1000))
+}
